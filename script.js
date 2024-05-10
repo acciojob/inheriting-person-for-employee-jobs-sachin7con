642b66625abd61c6cc1bf8a4dbedf4f25f5f0720
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-05-10 10:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-// complete this js code
-function Person(name, age) {}
+//SGN complete this js code
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
 
-function Employee(name, age, jobTitle) {}
+	this.greet = function() {
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
+}
+
+function Employee(name, age, jobTitle) {
+	this.name = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
+	this.jobGreet = function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old and my job title is ${this.jobTitle}.`);	
+	}
+
+}
+Employee.prototype = Object.create(Person.prototype);
+
 
 // Do not change code below this line
 window.Person = Person;
